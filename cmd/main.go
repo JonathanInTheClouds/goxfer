a59f9ef6ebc3464187d324eb2e3bb84481e9c55f
JonathanInTheClouds <48375298+JonathanInTheClouds@users.noreply.github.com>
2024-10-08 23:24:40 -0500

    first commit

--- cmd/main.go
+++ cmd/main.go
@@ -12,6 +12,9 @@ func main() {
 	// Define the flags for protocol, host, port, etc.
 	protocol := flag.String("protocol", "sftp", "Protocol to use for file transfer (e.g., sftp, scp, ftps)")
 	host := flag.String("host", "", "Remote server host")
+	port := flag.String("port", "22", "Remote server port")
+	username := flag.String("username", "", "SSH Username")
+	password := flag.String("password", "", "SSH Password")
 	file := flag.String("file", "", "File to transfer")
 	dest := flag.String("dest", "", "Destination path on the server")
 
@@ -19,34 +22,39 @@ func main() {
 	flag.Parse()
 
 	// Validate the flags
-	if *host == "" || *file == "" || *dest == "" {
-		fmt.Println("Error: host, file, and destination must be specified.")
+	if *host == "" || *port == "" || *file == "" || *dest == "" || *username == "" || *password == "" {
+		fmt.Println("Error: host, username, password, file, and destination must be specified.")
 		flag.Usage()
 		os.Exit(1)
 	}
-
 	fmt.Printf("Starting transfer using %s protocol...\n", *protocol)
 	fmt.Printf("Transferring file: %s to %s on %s\n", *file, *dest, *host)
 
 	// Call the transfer function
 	switch *protocol {
 	case "sftp":
-		err := transfer.SFTPTransfer(*file, *host, *dest)
+		err := transfer.SFTPTransfer(*username, *password, *host, *port, *file, *dest)
 		if err != nil {
 			fmt.Printf("Error transferring file: %v\n", err)
+			os.Exit(1)
 		}
 	case "scp":
 		err := transfer.SCPTransfer(*file, *host, *dest)
 		if err != nil {
 			fmt.Printf("Error transferring file: %v\n", err)
+			os.Exit(1)
 		}
 	case "ftps":
 		err := transfer.FTPSTransfer(*file, *host, *dest)
 		if err != nil {
 			fmt.Printf("Error transferring file: %v\n", err)
+			os.Exit(1)
 		}
 	default:
 		fmt.Printf("Error: unsupported protocol %s\n", *protocol)
 		os.Exit(1)
 	}
+
+	fmt.Println("File transfer complete.")
+	os.Exit(0)
 }
--- internal/transfer/sftp.go
+++ internal/transfer/sftp.go
@@ -1,10 +1,66 @@
 package transfer
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"time"
+
+	"github.com/pkg/sftp"
+	"golang.org/x/crypto/ssh"
+)
 
 // SFTPTransfer handles the SFTP file transfer logic
-func SFTPTransfer(file string, host string, dest string) error {
+func SFTPTransfer(username, password, host, port, file, dest string) error {
 	// Implement your SFTP logic here
-	fmt.Printf("SFTP transfer of file %s to %s on host %s\n", file, dest, host)
+	// fmt.Printf("SFTP transfer of file %s to %s on host %s\n", file, dest, host)
+	// return nil
+
+	config := &ssh.ClientConfig{
+		User: username,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(password),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		Timeout:         5 * time.Second,
+	}
+
+	conn, err := ssh.Dial("tcp", host+":"+port, config)
+	if err != nil {
+		return fmt.Errorf("failed to dial: %v", err)
+	}
+
+	defer conn.Close()
+
+	// Start an SFTP session
+	client, err := sftp.NewClient(conn)
+	if err != nil {
+		return fmt.Errorf("failed to create client: %v", err)
+	}
+
+	defer client.Close()
+
+	// Open the local file
+	srcFile, err := client.Open(file)
+	if err != nil {
+		return fmt.Errorf("failed to open source file: %v", err)
+	}
+
+	defer srcFile.Close()
+
+	// Open the destination file on the remote server
+	dstFile, err := client.Create(dest)
+	if err != nil {
+		return fmt.Errorf("failed to create destination file: %v", err)
+	}
+
+	defer dstFile.Close()
+
+	// Copy the file from the local system to the remote server
+	_, err = io.Copy(dstFile, srcFile)
+	if err != nil {
+		return fmt.Errorf("failed to copy file: %v", err)
+	}
+
+	fmt.Printf("File %s transferred to %s on %s\n", file, dest, host)
 	return nil
 }
